35963352db186ea7d867e6703efd4e5c1d3ff4a3
xs3esaw <97937377+xs3esaw@users.noreply.github.com>
2023-03-07 12:33:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let diena = 'otrdiena';
+const input1 = document.querySelector ('.js-teksts');
+const plusButton = document.querySelector('.js-class')
 
-const vards = 'linda';
 
-console.log(diena)
+console.log(input1);
+console.log(plusButton)
