43abefd41276da118d0ae96c4dcfe70420bf0e69
xs3esaw <97937377+xs3esaw@users.noreply.github.com>
2023-04-04 12:31:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const input1 = document.querySelector('.js-teksts');
-const plusButton = document.querySelector('.js-plus')
+const input1 = document.querySelector('.js-teksts1');
+const input2 = document.querySelector('.js-teksts2');
+const plusButton = document.querySelector('.js-plus');
+const minusButton = document.querySelector('.js-minus');
+const multiplyButton = document.querySelector('.js-multiply');
+const divideButton = document.querySelector('.js-divide');
+const result = document.querySelector('.js-result');
 
 const Add = (skaitlis1, skaitlis2) => {
-    return skaitlis1 + skaitlis2
+    return Number(skaitlis1) + Number(skaitlis2)
 }
 
 const Subtract = (skaitlis1, skaitlis2) => {
@@ -18,7 +23,6 @@ const Divide = (skaitlis1, skaitlis2) => {
 }
 
 plusButton.addEventListener('click', () => {
-    console.log("Mani nospieda")
+    result.innerHTML = Add(Number(input1.value), Number(input2.value))
 })
-
 
